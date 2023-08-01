f44a86ee9c7ecff455749791fd7bf5532e42f32b
Mark T <aydenthos@gmail.com>
2023-08-01 14:54:51 -0700

    updated basic port connectors, middleware for server.js

--- server.js
+++ server.js
@@ -14,3 +14,24 @@ const db = mysql.createConnection(
     console.log('Connected to the employee_db database.')
 );
 
+// Start the server
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+// Start server after DB connection
+db.connect(err => {
+    if (err) throw err;
+    console.log('Database connected.');
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+});
